e0403de49c355a9cefdefa4c8539137d258d32fe
jrAzael <160097480+jrAzael@users.noreply.github.com>
2024-02-16 22:13:51 -0600

    Add files via upload

---
+++ Documentos/c/SLenguaje.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    printf (" este es un texto \n");
+    printf (" que fue impreso desde c");
+    printf ("\n");
+    printf ( " ya soy todo un programador ");
+    printf ("\n");
+    return 0 ;
+
+}
---
+++ Documentos/c/priOpe.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+    // quiero imprimir el  numero 3 
+    printf("%d\n",3);
+    // quiero imprimir la suma de 3 mas 4 
+    printf("%d\n",3+4);
+    printf("la suma de %d + %d es = %d\n", 3,4,3+4);
+    return 0;
+}
