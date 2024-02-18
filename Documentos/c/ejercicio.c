76072e0b0ede978f333ed7b1ccdc59c730c7b92a
jrAzael <160097480+jrAzael@users.noreply.github.com>
2024-02-17 19:25:38 -0600

    Add files via upload

---
+++ Documentos/c/ejercicio.c
@@ -0,0 +1,12 @@
+# include <stdio.h> 
+int main (){
+int uno=3, dos=5, tres= 7;
+double d1= 28, d2= 6;
+//operacion de multiplicacion 
+printf ("el resultado de la multipliacion es %d", uno*dos*tres);
+printf("\n");
+// operacion de division
+printf ("el resultado de la division es %f", d1/d2);
+printf("\n");
+return 0;
+}
---
+++ Documentos/c/segundosVividos.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main (){
+    // 1 min = 60 seg
+    // i hora = 60 min
+    // 1 dia = 24 horas 
+    // 1 año = 365 dias 
+    int edad = 19;
+    //int seg = 60;
+    printf (" tu edad es %d",edad);
+    printf ("\n ");
+    printf("los segundos que has vivido son %d",edad*365*24*60*60);
+    printf ("\n ");
+    return 0;
+
+}
