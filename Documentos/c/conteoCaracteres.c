d3b309979afffde7f751434baa8e418a09cde031
jrAzael <160097480+jrAzael@users.noreply.github.com>
2024-02-18 23:52:35 -0600

    Add files via upload

---
+++ Documentos/c/conteoCaracteres.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+ 
+    // cuenta los caracteres de entrada 
+ main (){
+      long nc;
+      nc =0;
+      while (getchar()!=EOF)
+      {
+        ++nc;
+        printf("%1d\n",nc);
+        
+      }
+      
+ }
